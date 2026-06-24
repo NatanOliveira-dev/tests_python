207027873be7f45ae09f7aa9a9039a14f8ccfc32
Natan Oliveira <natanaeloliveira.work22@outlook.com>
2026-06-23 20:57:17 -0400

    Refactor test_one.py

---
+++ Set.py
@@ -0,0 +1,9 @@
+names = []
+
+for i in range(5):
+    name = input("Informe um nome: ")
+    names.append(name)
+
+for i in range(len(names)):
+    print(i, "-", names[i])
+
--- test_one.py
+++ test_one.py
@@ -1,9 +1,22 @@
-soma = 0
+def media_alunos():
+    soma = 0
 
-for i in range(4):
-    nota = float(input(f"Informe a {i + 1}° nota: "))
-    soma += nota
+    for i in range(4):
+        while True:
+            try:
+                notas = float(input(f"Digite a {i + 1}° nota: "))
 
-media = soma / 4
+                if notas < 0 or notas > 10:
+                    print("Nota inválida. Digite uma nota entre 0 e 10!")
+                else:
+                    soma += notas
+                    break
+            except ValueError:
+                print("Entrada inválida. Digite um número válido!")
 
-print(f"A média das notas é: {media:.1f}")
+    media = soma / 4
+    return media
+
+resultado = media_alunos()
+
+print(f"A média das notas dos alunos é: {resultado:.1f}")
